2f5760376cbc3356e2c510d0d9571cbb4dfb2c4c
Maxpaglu <yashkhetal2022.comp@mmcoe.edu.in>
2025-04-14 23:14:25 +0530

    Temporarily disabled home image to debug error

--- m1.py
+++ m1.py
@@ -70,8 +70,8 @@ def main():
         <h2 style='text-align: left;'>"Welcome to the Ultimate F1 Analysis Dashboard – Dive into real-time race insights, driver performance metrics, and historical trends that define the pinnacle of motorsport!"</h2>
         """, unsafe_allow_html=True)
 
-        home_image_path = r"C:\Users\DELL\Desktop\dsbda\images\FuJfLy2X0AEmetQ.jpg"
-        st.image(home_image_path, use_container_width=True)
+        # home_image_path = r"C:\Users\DELL\Desktop\dsbda\images\FuJfLy2X0AEmetQ.jpg"
+        # st.image(home_image_path, use_container_width=True)
 
     with tab2:
         st.subheader("Drivers Quali Comparisons")
